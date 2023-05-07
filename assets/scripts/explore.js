e7545e21f2db6d54226a18b9e1123759ebb3b2ed
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-07 11:33:36 -0700

    voice for different people work

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,27 +1,5 @@
 // explore.js
 
-/*const synth = window.speechSynthesis;
-var voice = [];
-function populateVoiceList() {
-  speechSynthesis.addEventListener("voiceschanged", () => {
-    const voice = synth.getVoices();
-
-    for (let i = 0; i < voice.length; i++) {
-      const option = document.createElement("option");
-      option.textContent = `${voice[i].name} (${voice[i].lang})`;
-
-      if (voice[i].default) {
-        option.textContent += " — DEFAULT";
-      }
-
-      option.setAttribute("data-lang", voice[i].lang);
-      option.setAttribute("data-name", voice[i].name);
-      document.getElementById("voice-select").appendChild(option);
-    }
-  });
-  return voice;
-}*/
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
@@ -29,13 +7,33 @@ function init() {
   var voices = [];
   var select = document.getElementById("voice-select");
   synth.onvoiceschanged = function() {
+    //get voices
     voices = synth.getVoices();
     for(let i = 0; i < voices.length; i++){
       const option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('data-name', voices[i].name);
       select.appendChild(option);
     }
-    var button = document.getElementsByTagName("button");
+  }
+  //get button to make it speak
+  var button = document.getElementsByTagName("button");
+  var text = document.getElementById("text-to-speak");
+  button[0].addEventListener('click', function(){
+    const utters = new SpeechSynthesisUtterance(text.value);
+    const selected = select.selectedOptions[0].getAttribute("data-name");
+    console.log(selected);
+    for (let i = 0; i < voices.length; i++) {
+      console.log(voices[i].name);
+      if (voices[i].name === selected) {
+        utters.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utters);
+  });
+};
+    /*var button = document.getElementsByTagName("button");
     var text = document.getElementById("text-to-speak");
     console.log(text);
     button[0].addEventListener('click', function(){
@@ -48,38 +46,23 @@ function init() {
       }
       synth.speak(utters);
     });
-  console.log(voices);
-  };
-    /*var button = document.getElementsByTagName("button");
-    var text = document.getElementById("text-to-speak");
-    console.log(text);
-    button[0].addEventListener('click', function(){
-      const utters = new SpeechSynthesisUtterance(text.value);
-      const selected = select.selectedOptions[0].getAttribute("data-name");
-      for (let i = 0; i < voices.length; i++) {
-        if (voices[i].name === selected) {
-          utterThis.voice = voices[i];
-        }
-      }
-      synth.speak(utters);
-    });*/
 };
-    /*button.addEventListener('click', function(){
+    button.addEventListener('click', function(){
       const utters = new SpeechSynthesisUtterance(text.value);
       const selected = voiceSelect.selectedOptions[0].getAttribute("data-name");
-    });*/
+    });
 
 
   
     
 
-    /*var button = document.getElementsByTagName("button");
+    var button = document.getElementsByTagName("button");
     var text = document.getElementsByName("text-to-speak");
-    console.log(text.value);*/
+    console.log(text.value);
 
   //})
   //when you press button, make it speak
-  /*HTMLElement.addEventListener('click', function(){
+  HTMLElement.addEventListener('click', function(){
     const utters = new SpeechSynthesisUtterance(text.value);
     const selected = voiceSelect.selectedOptions[0].getAttribute("data-name");
   })*/
