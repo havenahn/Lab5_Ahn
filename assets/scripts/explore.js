da03518bd656c41fc595b51651640a98cce4c92d
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-07 12:12:35 -0700

    finished

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -19,50 +19,25 @@ function init() {
   //get button to make it speak
   var button = document.getElementsByTagName("button");
   var text = document.getElementById("text-to-speak");
+  
   button[0].addEventListener('click', function(){
     const utters = new SpeechSynthesisUtterance(text.value);
     const selected = select.selectedOptions[0].getAttribute("data-name");
-    console.log(selected);
     for (let i = 0; i < voices.length; i++) {
-      console.log(voices[i].name);
       if (voices[i].name === selected) {
         utters.voice = voices[i];
         break;
       }
     }
+    var smiley = document.getElementsByTagName("img");
+    console.log(smiley);
+    utters.addEventListener('start', function () {
+      smiley[0].src = "assets/images/smiling-open.png";
+    });
+    utters.addEventListener('end', function(){
+      smiley[0].src = "assets/images/smiling.png";
+    })
     synth.speak(utters);
+
   });
 };
-    /*var button = document.getElementsByTagName("button");
-    var text = document.getElementById("text-to-speak");
-    console.log(text);
-    button[0].addEventListener('click', function(){
-      const utters = new SpeechSynthesisUtterance(text.value);
-      const selected = select.selectedOptions[0].getAttribute("data-name");
-      for (let i = 0; i < voices.length; i++) {
-        if (voices[i].name === selected) {
-          utterThis.voice = voices[i];
-        }
-      }
-      synth.speak(utters);
-    });
-};
-    button.addEventListener('click', function(){
-      const utters = new SpeechSynthesisUtterance(text.value);
-      const selected = voiceSelect.selectedOptions[0].getAttribute("data-name");
-    });
-
-
-  
-    
-
-    var button = document.getElementsByTagName("button");
-    var text = document.getElementsByName("text-to-speak");
-    console.log(text.value);
-
-  //})
-  //when you press button, make it speak
-  HTMLElement.addEventListener('click', function(){
-    const utters = new SpeechSynthesisUtterance(text.value);
-    const selected = voiceSelect.selectedOptions[0].getAttribute("data-name");
-  })*/
