3cbbf9882f5dcc7058a7a5449b7b86de9b3663a6
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-07 12:22:10 -0700

    expose set audio and pic

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var select = document.getElementById("horn-select");
+  var image = document.getElementsByTagName("img");
+  var audio = document.getElementsByTagName("audio")
+  
+  //change pics and set audio when selected
+  select.addEventListener('change', function(){
+    console.log(select.selectedOptions[0].value);
+    if(select.selectedOptions[0].value === "air-horn"){
+      image[0].src = "assets/images/air-horn.svg";
+      audio[0].src = "assets/audio/air-horn.mp3";
+    }
+    else if(select.selectedOptions[0].value === "car-horn"){
+      image[0].src = "assets/images/car-horn.svg";
+      audio[0].src = "assets/audio/car-horn.mp3";
+    }
+    else if(select.selectedOptions[0].value === "party-horn"){
+      image[0].src = "assets/images/party-horn.svg";
+      audio[0].src = "assets/audio/party-horn.mp3";
+    }
+  });
+
+  //change audio volume
 }
