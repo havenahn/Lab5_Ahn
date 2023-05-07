008aa7d84803bf1e674131fe3a5ee9228c5f4acb
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-06 21:35:22 -0700

    populated dropdown

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,15 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  let voices = [];
+  speechSynthesis.addEventListener("voiceschanged", () => {
+    voices = speechSynthesis.getVoices();
+    var select = document.getElementById("voice-select");
+    for(let i = 0; i < voices.length; i++){
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      select.appendChild(option);
+    }
+  })
 }
